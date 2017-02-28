9b1df21bd503e0c32e030eaf5ce0e96fe4c41c5c
ThiruKathir <ThiruKathir@users.noreply.github.com>
2017-02-28 21:37:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var articleOne = {
 
 function createTemplate (data) {
     var title = data.title;
-    var cate = data.date;
+    var date = data.date;
     var heading = data.heading;
     var content = data.content;
     
@@ -47,7 +47,7 @@ function createTemplate (data) {
                     ${heading}
                 </h3>
                 <div>
-                    ${data}
+                    ${date}
                 </div>
                 <div>
                     ${content}
