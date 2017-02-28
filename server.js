7baa8df1734bd3d709206cb94ecd2ebbefb15304
ThiruKathir <ThiruKathir@users.noreply.github.com>
2017-02-28 21:38:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var articleOne = {
 
 function createTemplate (data) {
     var title = data.title;
-    var date = data.date;
+    var cate = data.date;
     var heading = data.heading;
     var content = data.content;
     
