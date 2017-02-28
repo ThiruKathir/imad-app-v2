4c75f730d44924b0ec6d80a17a81eabdcd289bff
ThiruKathir <ThiruKathir@users.noreply.github.com>
2017-02-28 21:59:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,22 +6,43 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne = {
-    title: 'Article one | Thiru Kathir',
-    heading: 'Article One',
-    date: 'Feb 22, 2017',
-    content: `
-    <p>
-                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>
-              <p>
-                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>
-              <p>
-                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>`
-};  
-
+var articles = {
+            'article-one' : {
+                title: 'Article one | Thiru Kathir',
+                heading: 'Article One',
+                date: 'Feb 22, 2017',
+                content: `
+                <p>
+                            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                        </p>
+                          <p>
+                            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                        </p>
+                          <p>
+                            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                        </p>`
+            },
+            'article-two' : { 
+                title: 'Article Two | Thiru Kathir',
+                heading: 'Article One',
+                date: 'Feb 22, 2017',
+                content: `
+                <p>
+                            This is the content for my second article. 
+                        </p>`
+            },
+            'article-three' : {
+                title: 'Article Three | Thiru Kathir',
+                heading: 'Article Three',
+                date: 'Feb 22, 2017',
+                content: `
+                <p>
+                            This is the content for my Third     article. 
+                        </p>`
+                
+            }
+};
+    
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
@@ -63,16 +84,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res){
-res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function(req, res){
+    // article name == article-one
+    //  articles [articleName] == {} content object for articel one
+    var articleName = req.params.articleName;
+res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
