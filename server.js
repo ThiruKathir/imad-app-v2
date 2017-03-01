389ee41605cbc4b4e07cb9dee51810bd512ee912
ThiruKathir <ThiruKathir@users.noreply.github.com>
2017-03-01 22:33:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,11 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var counter =0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1
+    res.send(counter.toString())
+});
 app.get('/:articleName', function(req, res){
     // article name == article-one
     //  articles [articleName] == {} content object for articel one
