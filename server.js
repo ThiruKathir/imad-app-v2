6feb68e2ffd89fa5732f8bfd82086b27d7bce2ed
ThiruKathir <ThiruKathir@users.noreply.github.com>
2017-03-05 15:02:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/counter', function (req, res) {
 var names= [];
 app.get('/submit-name', function(req,res) {
   // get the name from the request object
-  var name= req.qurey.name
+  var name= req.query.name
   names.push(name);
   //JSON: Javascript Object Notation
   res.send(JSON.stringify(names));
